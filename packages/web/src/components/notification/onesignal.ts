eef9d295472b42f7620f99a9709e013db46ce373
SonuKumar81800 <sonu.patna0808@gmail.com>
2021-12-29 13:44:19 +0530

    Integrating Push Notification with INAPP notifications

--- packages/web/src/components/notification/onesignal.ts
+++ packages/web/src/components/notification/onesignal.ts
@@ -7,11 +7,17 @@ export const useOneSignal = () => {
   useEffect(() => {
     OneSignal.init({
       appId: 'd7a822b5-a821-460f-a60a-86d08d19e8f0', //deployed api key will be replaced by env
+      safari_web_id: 'web.onesignal.auto.235723f6-f2ef-49f2-bb5e-e966332d4e54',
+      notifyButton: {
+        enable: true,
+      },
+      // allowLocalhostAsSecureOrigin: true, // this is for local testing only
     });
   }, []);
   useEffect(() => {
     if (authenticated) {
-      OneSignal.setExternalUserId(attributes.sub);
+      OneSignal.setExternalUserId(attributes['custom:_id']);
+      console.log('subs: ' + attributes['custom:_id']);
     } else {
       OneSignal.removeExternalUserId();
     }
