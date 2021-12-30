28cc887c68d1c9cd1a3023b5acf687519721a851
SonuKumar81800 <sonu.patna0808@gmail.com>
2021-12-30 16:25:08 +0530

    Disabling PWA for testing Push Notification

--- packages/web/src/components/notification/onesignal.ts
+++ packages/web/src/components/notification/onesignal.ts
@@ -7,10 +7,12 @@ export const useOneSignal = () => {
   useEffect(() => {
     OneSignal.init({
       appId: 'd7a822b5-a821-460f-a60a-86d08d19e8f0', //deployed api key will be replaced by env
+      // appId: '7f0f54e4-44c9-44a1-a143-452fcef5ef9b', // Local testing
+
       safari_web_id: 'web.onesignal.auto.235723f6-f2ef-49f2-bb5e-e966332d4e54',
-      notifyButton: {
-        enable: true,
-      },
+      // notifyButton: {
+      //   enable: true,
+      // },
       // allowLocalhostAsSecureOrigin: true, // this is for local testing only
     });
   }, []);
